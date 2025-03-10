067a6de968d8e6b19f6d6eabace8791568bb6088
leosalgado <leonardosalgado@protonmail.com>
2025-03-10 16:39:23 -0300

    Business Layer Implementation

---
+++ src/main/java/com/teste/api_santander/service/UserService.java
@@ -0,0 +1,10 @@
+package com.teste.api_santander.service;
+
+import com.teste.api_santander.domain.model.User;
+
+public interface UserService {
+    User findById(Long id);
+
+    User create(User userToCreate);
+
+}
---
+++ src/main/java/com/teste/api_santander/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.teste.api_santander.service.impl;
+
+import com.teste.api_santander.domain.model.User;
+import com.teste.api_santander.domain.repository.UserRepository;
+import com.teste.api_santander.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())){
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
