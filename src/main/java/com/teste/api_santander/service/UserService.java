ee9fd08e7ae4de28dd70acdf34e7e4c6efda29e9
leosalgado <leonardosalgado@protonmail.com>
2025-03-10 17:47:14 -0300

    Controller Layer Implementation

--- src/main/java/com/teste/api_santander/service/UserService.java
+++ src/main/java/com/teste/api_santander/service/UserService.java
@@ -3,8 +3,8 @@ package com.teste.api_santander.service;
 import com.teste.api_santander.domain.model.User;
 
 public interface UserService {
+
     User findById(Long id);
 
     User create(User userToCreate);
-
 }
